f77cc56ca8dd0c03f78fd012bfef38717ecf8a9f
Jayju21 <ampemdarkogrp@gmail>
2023-02-18 18:08:25 -0800

    mutilingual

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
- * Return: Always 1 (Success)
+ * main - print quote
+ *
+ * Description: 'print dora's quote'
+ *
+ * Return: Always 1 (success)
  */
-
 
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
