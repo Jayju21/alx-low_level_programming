0b5d6b46d67faf519965851f5bcb75ae27c1b0fc
Jayju21 <ampemdarkogrp@gmail>
2023-02-24 18:09:45 -0800

    102 fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -16,7 +16,7 @@ int main(void)
 	if (i == 0)
 	printf("%ld", j);
 	else if (i == 1)
-	 printf(", %ld", k);
+	printf(", %ld", k);
 	else
 	{
 	k += j;
@@ -25,6 +25,7 @@ int main(void)
 	}
 	++i;
 	}
+
 	printf("\n");
 	return (0);
 }
