4dd16e9abca98ce1120c3e4c8eee1dad1989088d
Jayju21 <ampemdarkogrp@gmail>
2023-03-09 11:16:08 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,12 +8,12 @@
  */
 int power_operation(int n, int c)
 {
-        if (c % (n / c) == 0)
-        {
-                if (c * (n / c) == n)
-                        return (c);
-                else
-		return (-1);
+	if (c % (n / c) == 0)
+	{
+		if (c * (n / c) == n)
+			return (c);
+		else
+			return (-1);
 	}
 	return (0 + power_operation(n, c + 1));
 }
